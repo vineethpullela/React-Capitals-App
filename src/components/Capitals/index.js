0efc6f49f2987533c06eb304e245ecbb195eeaea
vineeth <15072cm031@gmail.com>
2022-12-13 08:42:44 +0000

    ready to test

---
+++ src/components/Capitals/index.js
@@ -0,0 +1,76 @@
+import {Component} from 'react'
+import './index.css'
+
+const countryAndCapitalsList = [
+  {
+    id: 'NEW_DELHI',
+    capitalDisplayText: 'New Delhi',
+    country: 'India',
+  },
+  {
+    id: 'LONDON',
+    capitalDisplayText: 'London',
+    country: 'United Kingdom',
+  },
+  {
+    id: 'PARIS',
+    capitalDisplayText: 'Paris',
+    country: 'France',
+  },
+  {
+    id: 'KATHMANDU',
+    capitalDisplayText: 'Kathmandu',
+    country: 'Nepal',
+  },
+  {
+    id: 'HELSINKI',
+    capitalDisplayText: 'Helsinki',
+    country: 'Finland',
+  },
+]
+
+class Capitals extends Component {
+  state = {activeCapitalId: countryAndCapitalsList[0].id}
+
+  changeCapital = event => {
+    this.setState({activeCapitalId: event.target.value})
+  }
+
+  getCountry = () => {
+    const {activeCapitalId} = this.state
+    const activeCountryAndCapital = countryAndCapitalsList.find(
+      eachCapital => eachCapital.id === activeCapitalId,
+    )
+
+    return activeCountryAndCapital.country
+  }
+
+  render() {
+    const {activeCapitalId} = this.state
+    const country = this.getCountry()
+    return (
+      <div className="app-container">
+        <div className="capital-card-container">
+          <h1 className="main-heading">Countries And Capitals</h1>
+          <div className="option-container">
+            <select
+              className="country-list"
+              value={activeCapitalId}
+              onChange={this.changeCapital}
+            >
+              {countryAndCapitalsList.map(each => (
+                <option key={each.id} value={each.id}>
+                  {each.capitalDisplayText}
+                </option>
+              ))}
+            </select>
+            <p className="question-text">is capital of which country?</p>
+          </div>
+          <p className="country-name">{country}</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Capitals
